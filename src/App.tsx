e39e6e2b85334f517515e3e583f86bcd33ac4515
Luke Olney <lukeolney@gmail.com>
2019-09-22 13:24:58 -0400

    Allow starting and stopping

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
-import VanillaCompletionGrid from "./CompletionGrid/Vanilla";
-import GpgpuCompletionGrid from "./CompletionGrid/GPGPU";
+import CompletionGrid from "./CompletionGrid/BaseWorker";
+import GpuWorker from './workers/gpuworker.worker';
+import VanillaWorker from './workers/worker.worker';
 import logo from "./logo.svg";
 import "./App.css";
 
 const App: React.FC = () => {
   const [isStarted, setIsStarted] = useState(false);
-  const start = () => setIsStarted(true);
+  const [workersCompleted, setWorkersCompleted] = useState(0);
+  const start = () => {
+    setIsStarted(true);
+    setWorkersCompleted(0);
+  }
+  const stop = () => {
+    setIsStarted(false);
+    setWorkersCompleted(0);
+  }
+  const setDone = () => {
+    if(workersCompleted === 1) stop();
+    else setWorkersCompleted(state => state + 1);
+  }
 
   return (
     <header className="root">
       <h1>Demo</h1>
-      <button disabled={isStarted} onClick={start}>Start</button>
+      <button onClick={isStarted ? stop : start}>{isStarted ? "Stop" : "Start"}</button>
       <div className="demo-container">
         <div className="grid-wrapper">
           <h2>Vanilla Javascript</h2>
-          <VanillaCompletionGrid isStarted={isStarted}/>
+          <CompletionGrid isStarted={isStarted} setDone={setDone} Worker={VanillaWorker}/>
         </div>
         <div className="grid-wrapper">
           <h2>Uses the GPU</h2>
-          <GpgpuCompletionGrid isStarted={isStarted}/>
+          <CompletionGrid isStarted={isStarted} setDone={setDone} Worker={GpuWorker} />
         </div>
       </div>
     </header>
--- src/CompletionGrid/BaseWorker/index.tsx
+++ src/CompletionGrid/BaseWorker/index.tsx
@@ -4,23 +4,42 @@ import { CompletionGrid } from "../Base";
 
 type CompletionGridWorkerProps = {
   isStarted: boolean;
-  worker: Worker;
+  setDone: () => void,
+  Worker: Worker;
 };
 
 const CompletionGridWorker: React.FC<CompletionGridWorkerProps> = ({
   isStarted,
-  worker
+  setDone,
+  Worker
 }) => {
   const [completedCount, updateCount] = useState(0);
+  const [worker, setWorker] = useState<Worker | undefined>();
 
   useEffect(() => {
-    if(isStarted) worker.postMessage("start");
+    if(isStarted && !worker) {
+      // @ts-ignore
+      const worker = new Worker();
+      worker.postMessage("start");
+      setWorker(worker);
+    }
+    else if(!isStarted && worker) {
+      worker.terminate();
+      setWorker(undefined);
+      updateCount(0);
+    }
   }, [worker, isStarted]);
 
   useEffect(() => {
-    worker.onmessage = () =>
+    if(worker) worker.onmessage = () =>
       updateCount((completedCount: number) => completedCount + 1);
   }, [worker]);
+
+  useEffect(() => {
+    if (completedCount === 100)
+      setDone();
+  }, [completedCount]);
+
 
   return <CompletionGrid completedCount={completedCount} totalCount={100} />;
 };
